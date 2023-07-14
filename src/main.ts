faa7a6c12f020160fde16f2c2ec08b18c35b6b7e
kimhyun5u <gustndlfrl@gmail.com>
2023-07-14 18:24:06 +0900

    update: add checker process.* exist

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  process.on('SIGINT', () => {
-    app.close();
-    process.exit(0);
-  });
+  if (process.on) {
+    process.on('SIGINT', () => {
+      app.close();
+      process.exit(0);
+    });
+  }
 
   const app = await NestFactory.create(AppModule);
   await app.listen(3000, () => {
-    process.send('ready');
+    if (process.send) {
+      process.send('ready');
+    }
   });
 }
 bootstrap();
