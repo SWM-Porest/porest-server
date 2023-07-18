39895a7938fab560ce819b4a8643f6dc20f9a1cf
kimhyun5u <gustndlfrl@gmail.com>
2023-07-18 13:56:17 +0900

    feat: add project config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [RestaurantsModule, MongooseModule.forRoot('mongodb://localhost/nest', { dbName: 'dbname' })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+      }),
+      isGlobal: true,
+      cache: true,
+    }),
+    RestaurantsModule, MongooseModule.forRoot('mongodb://localhost/nest', { dbName: 'dbname' })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
