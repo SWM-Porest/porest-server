66941db1573775c6c6719e359a323248846068e4
kimhyun5u <gustndlfrl@gmail.com>
2023-07-18 13:58:10 +0900

    feat: add controller logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,31 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.dev',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      ignoreEnvFile: process.env.NODE_ENV === 'dev',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
       }),
       isGlobal: true,
       cache: true,
     }),
-    RestaurantsModule, MongooseModule.forRoot('mongodb://localhost/nest', { dbName: 'dbname' })
+    RestaurantsModule,
+    MongooseModule.forRoot(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/nest`, { dbName: 'dbname' }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
