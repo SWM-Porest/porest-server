139fc52ec348aad7d1775cadfe2735153db6741d
kimhyun5u <gustndlfrl@gmail.com>
2023-07-20 14:15:35 +0900

    feat: add basic auth to post delete patch method

---
+++ src/auth/basic-auth.strategy.ts
@@ -0,0 +1,22 @@
+import { BasicStrategy as Strategy } from 'passport-http';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class BasicAuthStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      passReqToCallback: true,
+    });
+  }
+  async validate(req, username: string, password: string): Promise<boolean> {
+    if (
+      this.configService.get('HTTP_BASIC_USER') === username &&
+      this.configService.get('HTTP_BASIC_PASS') === password
+    ) {
+      return true;
+    }
+    throw new UnauthorizedException();
+  }
+}
