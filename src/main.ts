26e1938f89c79d5f7684a4a781a26d5f69d0bd0e
kimhyun5u <gustndlfrl@gmail.com>
2023-07-29 20:54:07 +0900

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   if (process.on) {
@@ -14,6 +15,15 @@ async function bootstrap() {
   if (process.env.NODE_ENV === 'dev') {
     app.enableCors();
   }
+
+  const config = new DocumentBuilder()
+    .setTitle('PocketRestaurant API')
+    .setDescription('PocketRestaurant API description')
+    .setVersion('1.0')
+    .addTag('pocekrestaurant')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT, () => {
     if (process.send) {
