c151b653f4ade70fe20195b9519c7b5f5b806701
kimhyun5u <gustndlfrl@gmail.com>
2023-07-29 20:42:54 +0900

    update: remove unused dependency

--- src/restaurants/restaurants.repository.ts
+++ src/restaurants/restaurants.repository.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Menu, Restaurant } from './schemas/restaurants.schema';
 import { Model } from 'mongoose';
