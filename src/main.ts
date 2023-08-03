358f215d3cad2796671c05bfbe4677e73cbce604
kimhyun5u <gustndlfrl@gmail.com>
2023-08-03 18:39:20 +0900

    update: update cors list

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://pocketrestaurant.net'],
+    origin: ['http://localhost:3000', 'http://pocketrestaurant.net', 'https://pocketrestaurant.net'],
     credentials: true,
   });
 
