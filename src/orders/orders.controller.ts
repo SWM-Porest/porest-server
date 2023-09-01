bbea2950ba86657a0f0ab1af01d31ba5c3291a44
midhyun <kfree912@gmail.com>
2023-09-01 20:20:43 +0900

    feat: Auth 권한 부여

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,32 +1,45 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 import { OrdersService } from './orders.service';
 import { CreateOrdersDto } from './dto/createOrders.dto';
 import { UpdateOrdersDto } from './dto/updateOrders.dto';
 import { ParseObjectIdPipe } from 'src/common/pipes/parse-object-id.pipe';
 import { Types } from 'mongoose';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { Roles } from 'src/auth/decorator/roles.decorator';
+import { UserRole } from 'src/auth/schemas/user.schema';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
 
 @ApiTags('orders')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @ApiForbiddenResponse({
+    status: 403,
+    description: '주문 권한이 없습니다.',
+  })
+  @Roles(UserRole.USER)
   @Post()
   async createOrder(@Body() createOrdersDto: CreateOrdersDto) {
     const data = await this.ordersService.createOrder(createOrdersDto);
     return data;
   }
 
+  @Roles(UserRole.RESTAURANT_MANAGER)
   @Patch()
   async updateOrder(@Body() updateOrdersDto: UpdateOrdersDto) {
     return await this.ordersService.updateOrder(updateOrdersDto);
   }
 
+  @Roles(UserRole.RESTAURANT_MANAGER)
   @Delete('/delete')
   async deleteOrder() {
     return await this.ordersService.deleteOrder();
   }
 
+  @Roles(UserRole.USER)
   @Get('/:id')
   async getOrder(@Param('id') id: string) {
     try {
@@ -36,6 +49,7 @@ export class OrdersController {
     }
   }
 
+  @Roles(UserRole.USER)
   @Get('/user/:id')
   async getOrdersByUser(
     @Param('id', ParseObjectIdPipe) id: Types.ObjectId,
@@ -46,6 +60,7 @@ export class OrdersController {
     return await this.ordersService.getOrdersByUser(id, page, pageSize, sort);
   }
 
+  @Roles(UserRole.RESTAURANT_MANAGER)
   @Get('/restaurant/:id')
   async getOrdersByRestaurant(
     @Param('id', ParseObjectIdPipe) id: Types.ObjectId,
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,10 +4,13 @@ import { OrdersService } from './orders.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrderSchema } from './schemas/orders.schema';
 import { OrdersRepository } from './orders.repository';
+import { AuthModule } from 'src/auth/auth.module';
+import { AuthService } from 'src/auth/auth.service';
+import { UsersService } from 'src/auth/user.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Orders', schema: OrderSchema }])],
+  imports: [MongooseModule.forFeature([{ name: 'Orders', schema: OrderSchema }]), AuthModule],
   controllers: [OrdersController],
-  providers: [OrdersService, OrdersRepository],
+  providers: [OrdersService, OrdersRepository, AuthService, UsersService],
 })
 export class OrdersModule {}
