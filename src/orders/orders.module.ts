dae75bf45428086761fc2472d001f320ebbceb17
midhyun <kfree912@gmail.com>
2023-09-25 18:23:38 +0900

    update: dependency injection orders

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -7,10 +7,12 @@ import { OrdersRepository } from './orders.repository';
 import { AuthModule } from 'src/auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
 import { UsersService } from 'src/auth/user.service';
+import { RestaurantsModule } from 'src/restaurants/restaurants.module';
+import { RestaurantsService } from 'src/restaurants/restaurants.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Orders', schema: OrderSchema }]), AuthModule],
+  imports: [MongooseModule.forFeature([{ name: 'Orders', schema: OrderSchema }]), AuthModule, RestaurantsModule],
   controllers: [OrdersController],
-  providers: [OrdersService, OrdersRepository, AuthService, UsersService],
+  providers: [OrdersService, OrdersRepository, AuthService, UsersService, RestaurantsService],
 })
 export class OrdersModule {}
