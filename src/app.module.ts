1e40062949f222b1b0110c32d782ac5101c1d9f3
kimhyun5u <gustndlfrl@gmail.com>
2023-11-10 17:29:37 +0900

    feat: add Cache -  GET /restaurants/:id - POST /restaurants/:id

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
@@ -9,6 +9,8 @@ import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
 import { WaitingsModule } from './waitings/waitings.module';
 import { TablesModule } from './tables/tables.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
   imports: [
@@ -27,6 +29,50 @@ import { TablesModule } from './tables/tables.module';
     OrdersModule,
     WaitingsModule,
     TablesModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      clusterConfig: {
+        nodes: [
+          {
+            host: process.env.REDIS_HOST_1,
+            port: 6379,
+          },
+          {
+            host: process.env.REDIS_HOST_2,
+            port: 6379,
+          },
+          {
+            host: process.env.REDIS_HOST_3,
+            port: 6379,
+          },
+          {
+            host: process.env.REDIS_HOST_4,
+            port: 6379,
+          },
+          {
+            host: process.env.REDIS_HOST_5,
+            port: 6379,
+          },
+          {
+            host: process.env.REDIS_HOST_6,
+            port: 6379,
+          },
+          {
+            host: process.env.REDIS_HOST_7,
+            port: 6379,
+          },
+          {
+            host: process.env.REDIS_HOST_8,
+            port: 6379,
+          },
+          {
+            host: process.env.REDIS_HOST_9,
+            port: 6379,
+          },
+        ],
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
